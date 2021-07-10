d51446248533be36f94e5630f3eb44b7d11c6816
Stergios Hetelekides <hetelek@gmail.com>
2021-07-10 02:08:16 -0700

    add test image for generalization check

--- train.py
+++ train.py
@@ -37,18 +37,33 @@ def load_random_image():
     g = torch.tensor(g).reshape((32, 32))
     b = torch.tensor(b).reshape((32, 32))
     img = torch.stack((r, g, b))
+    img = img.type(torch.float)
 
     return img
+
+def load_disk_image(name):
+    img_data = Image.open(name)
+    img_arr = np.array(img_data)
+    img_arr = torch.tensor(img_arr)
+    img_arr = img_arr.transpose(0, 2)
+    img_arr = img_arr.transpose(1, 2)
+    img_arr = img_arr.type(torch.float)
+    assert img_arr.shape == (3, 32, 32)
+    return img_arr
 
 # for _ in range(0, 100):
 #     i, _ = load_random_image()
 #     time.sleep(1)
 
-i = load_random_image()
-i = i / 255.
+images = []
+for _ in range(0, 100):
+    i = load_random_image()
+    i = i / 255.
+    images.append(i)
 
-batch = i.clone().detach().type(torch.float) # torch.tensor(i, dtype=torch.float)
-batch = torch.unsqueeze(batch, 0)
+batch = torch.stack(images)
+# batch = batch.type(torch.float) # torch.tensor(i, dtype=torch.float)
+# batch = torch.unsqueeze(batch, 0)
 
 c = nn.Conv2d(3, 3, 5, padding=(2, 2))
 # c2 = nn.Conv2d(6, 10, 5, padding=(2, 2))
@@ -58,7 +73,7 @@ c5 = nn.Conv2d(3, 3, 5, padding=(2, 2))
 d = nn.Dropout(p=0.4)
 
 def forward(x):
-    o = c(x)
+    o = d(c(x))
     # o = c2(o)
     # o = d(c3(o))
     # o = c4(o)
@@ -72,16 +87,22 @@ for a in all:
 
 # for a in all:
 #     params.extend(list(a.parameters()))
-optimizer = torch.optim.SGD(params, lr=1e-5, momentum=0.9)
+optimizer = torch.optim.SGD(params, lr=1e-8, momentum=0.9)
 
-for _ in range(0, 10000):
+for epoch in range(0, 10000):
     o = forward(batch)
-    loss = torch.sum(torch.abs(o - i))
+    loss = torch.sum(torch.abs(o - batch))
     print(loss)
     
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-save_image(o * 255., name='final.png')
+    if epoch % 10 == 0:
+        for i in range(0, o.shape[0]):
+            save_image(o[i] * 255., name='imgs/{}.png'.format(i))
 
+        i = load_disk_image('test.png')
+        i = i.unsqueeze(0)
+        o = forward(i)
+        save_image(o)
