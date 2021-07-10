faa2cbaa7d2fff4371ae24507c70f5a049509a4a
Stergios Hetelekides <hetelek@gmail.com>
2021-07-10 03:39:03 -0700

    random changes

--- train.py
+++ train.py
@@ -7,6 +7,11 @@ import torch
 from torch import optim
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.optim import optimizer
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
 
 
 file = '/Users/hetelek/Downloads/cifar-10-batches-py/data_batch_1'
@@ -27,8 +32,9 @@ def save_image(tensor, name='img_1.png'):
     img = Image.fromarray(np_array)
     img.save(name)
 
-def load_random_image():
-    i = np.random.randint(0, len(dict[b'data']))
+def load_random_image(force_index=None):
+    i = np.random.randint(0, len(dict[b'data'])) if not force_index else force_index
+    label = dict[b'filenames'][i].decode('utf-8')
     r = dict[b'data'][i][:1024]
     g = dict[b'data'][i][1024:1024+1024]
     b = dict[b'data'][i][1024+1024:]
@@ -39,7 +45,7 @@ def load_random_image():
     img = torch.stack((r, g, b))
     img = img.type(torch.float)
 
-    return img
+    return img, label
 
 def load_disk_image(name):
     img_data = Image.open(name)
@@ -57,13 +63,15 @@ def load_disk_image(name):
 
 def load_batch(batch_size):
     images = []
-    for _ in range(0, batch_size):
-        i = load_random_image()
+    labels = []
+    for z in range(0, batch_size):
+        i, l = load_random_image(z)
         i = i / 255.
         images.append(i)
+        labels.append(l)
 
     batch = torch.stack(images)
-    return batch
+    return batch, labels
 
 # batch = batch.type(torch.float) # torch.tensor(i, dtype=torch.float)
 # batch = torch.unsqueeze(batch, 0)
@@ -72,31 +80,51 @@ c = nn.Conv2d(3, 3, 5, padding=(2, 2))
 # c2 = nn.Conv2d(6, 10, 5, padding=(2, 2))
 # c3 = nn.Conv2d(10, 50, 5, padding=(2, 2))
 # c4 = nn.Conv2d(50, 30, 5, padding=(2, 2))
+c_full = nn.Linear(32, 200)
+c_full2 = nn.Linear(200, 200)
+c_full3 = nn.Linear(200, 32)
+# c_full = nn.Linear(32, 960)
 c5 = nn.Conv2d(3, 3, 5, padding=(2, 2))
 d = nn.Dropout(p=0.4)
 
 def forward(x):
-    o = c(x)
+    o = c_full(x) #c(x)
+    o = c_full2(o)
+    o = c_full3(o)
     # o = c2(o)
     # o = d(c3(o))
     # o = c4(o)
-    return c5(o)
+    # print(o.shape)
+    return o #c5(d(c_full(o)))
 
 # all = [d, c, c2, c3, c4, c5]
-all = [d, c, c5]
+all = [d, c, c5, c_full, c_full2, c_full3]
 params = []
 for a in all:
     params += list(a.parameters())
 
-# for a in all:
-#     params.extend(list(a.parameters()))
-optimizer = torch.optim.SGD(params, lr=1e-8, momentum=0.9)
+# optimizer = torch.optim.SGD(params, lr=1e-6, momentum=0.9)
+optimizer = torch.optim.Adam(params, lr=1e-2)
 
 for epoch in range(0, 10000):
-    batch = load_batch(100)
+    batch, labels = load_batch(1000)
     o = forward(batch)
-    loss = torch.sum(torch.abs(o - batch))
+    loss = torch.abs(o - batch)
+    # print(loss.shape)
+    # max = torch.argmax(loss)
+    # print(max)
+    # print(loss)
+    loss2 = torch.sum(loss, 1)
+    loss2 = torch.sum(loss2, 1)
+    loss2 = torch.sum(loss2, 1)
+    max = torch.argmax(loss2)
+    min = torch.argmin(loss2)
+    # print(labels[x])
+    # print()
+
+    loss = torch.sum(loss)
     print(loss)
+    writer.add_scalar("Loss/train", loss, epoch)
     
     optimizer.zero_grad()
     loss.backward()
@@ -110,3 +138,9 @@ for epoch in range(0, 10000):
         i = i.unsqueeze(0)
         o = forward(i)
         save_image(o)
+
+        save_image(batch[max] * 255., name='img_high_loss.png')
+        save_image(batch[min] * 255., name='img_low_loss.png')
+
+
+writer.close()
