c7cd8abebf2daf489822223a578b006f82d6b5d9
Stevie Hetelekides <hetelek@gmail.com>
2025-03-02 14:02:49 -0800

    benchmark samsung T7 speeds

--- deepseek/main.py
+++ deepseek/main.py
@@ -4,10 +4,35 @@ import time
 
 device = "mps" if torch.backends.mps.is_available() else "cpu"
 
+'''
+/Users/hetelek/Desktop/:
+Run 1 (safetensors files 00001 - 00008)
+Total loaded: 35423620480
+Loading time: 41326.16ms
+Load speed: 817.46 MB/s
+
+Run 2 (safetensors files 00001 - 00008)
+Total loaded: 35423620480
+Loading time: 36594.41ms
+Load speed: 923.16 MB/s
+
+--------
+
+/Volumes/T7/DeepSeek-V3-671B/:
+Run 1 (safetensors files 00001 - 00008):
+Total loaded: 35423620480
+Loading time: 75228.20ms
+Load speed: 449.07 MB/s
+
+Run 2 (safetensors files 00001 - 00008):
+Total loaded: 35423620480
+Loading time: 75395.60ms
+Load speed: 448.07 MB/s
+'''
 
 # from deepseek: 5G file
 all_weights_size = 0
-safetensors_paths = [f"/Users/hetelek/Desktop/model-0000{i}-of-000163.safetensors" for i in range(1, 9)]
+safetensors_paths = [f"/Volumes/T7/DeepSeek-V3-671B/model-0000{i}-of-000163.safetensors" for i in range(1, 9)]
 file_to_weights_size = {}
 for safetensors_path in safetensors_paths:
     with open(safetensors_path, 'rb') as f:
